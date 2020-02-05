c46f75c24c81c82c94985296b7d1a7b7ee0c6542
khalayilwanga <truphena.lwanga@strathmore.edu>
2020-02-05 13:14:48 +0300

    Built react client

---
+++ client/precache-manifest.977de532e048381aad173d310cc38dd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da8f4a69b7ed02902bd81ce206c8a96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251cd205302fd01270d0",
+    "url": "/static/css/main.c96599c0.chunk.css"
+  },
+  {
+    "revision": "7c5b31e70f8345f61f2f",
+    "url": "/static/js/2.08d39e56.chunk.js"
+  },
+  {
+    "revision": "db7136bbaacd8185356c0c997d57dd27",
+    "url": "/static/js/2.08d39e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251cd205302fd01270d0",
+    "url": "/static/js/main.ea6fef9c.chunk.js"
+  },
+  {
+    "revision": "8d085c7f6c50bb73613a",
+    "url": "/static/js/runtime-main.d2faf1d9.js"
+  },
+  {
+    "revision": "06363609eb0786c8132cd7b6e46ee419",
+    "url": "/static/media/naasilogo.06363609.png"
+  }
+]);
